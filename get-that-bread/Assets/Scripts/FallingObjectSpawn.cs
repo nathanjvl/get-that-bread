2271818ab425e83f31fbd7aceb3b3b9612ff4bd0
tim-kashani <93025858+tim-kashani@users.noreply.github.com>
2021-10-23 12:00:30 -0700

    added falling object spawn script

---
+++ get-that-bread/Assets/Scripts/FallingObjectSpawn.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectSpawn : MonoBehaviour
+{
+    //x axis range of object spawning
+    public float range;
+
+    public GameObject[] collectables;
+
+    private float random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sets random value
+        random = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if random timer is 0 spawn object
+
+        if (random > 0)
+        {
+            random -= Time.deltaTime;
+        } else
+        {
+            random = Random.Range(1, 5);
+        }
+    }
+}
