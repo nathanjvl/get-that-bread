be653082edc003e0abcfbcc56c82ce02e504db3d
tim-kashani <93025858+tim-kashani@users.noreply.github.com>
2021-10-23 11:43:09 -0700

    updated script

--- get-that-bread/Assets/Scripts/Player.cs
+++ get-that-bread/Assets/Scripts/Player.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //declares rigidbody
     private Rigidbody2D r;
 
+    //declares speed
     public float speed;
 
     // Start is called before the first frame update
@@ -17,12 +19,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.UpArrow))
+        //change velocity if player pushes certain button
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             r.velocity = new Vector2(speed * -1, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             r.velocity = new Vector2(speed, 0);
         }
