23a9e5acf74a004b3f77b4d1193e801f0d4acf78
tim-kashani <93025858+tim-kashani@users.noreply.github.com>
2021-10-23 12:35:29 -0700

    updated player script

--- get-that-bread/Assets/Scripts/Player.cs
+++ get-that-bread/Assets/Scripts/Player.cs
@@ -10,10 +10,14 @@ public class Player : MonoBehaviour
     //declares speed
     public float speed;
 
+    private GameObject stage1Manager;
+
     // Start is called before the first frame update
     void Start()
     {
         r = GetComponent<Rigidbody2D>();
+
+        stage1Manager = GameObject.Find("Stage 1 Manager");
     }
 
     // Update is called once per frame
@@ -29,6 +33,29 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             r.velocity = new Vector2(speed, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Milk")
+        {
+            stage1Manager.GetComponent<Stage1Manager>().milk_count++;
+        }
+
+        if (collision.transform.tag == "Flour")
+        {
+            stage1Manager.GetComponent<Stage1Manager>().flour_count++;
+        }
+
+        if (collision.transform.tag == "Yeast")
+        {
+            stage1Manager.GetComponent<Stage1Manager>().yeast_count++;
+        }
+
+        if (collision.transform.tag == "Trash")
+        {
+            stage1Manager.GetComponent<Stage1Manager>().trash_count++;
         }
     }
 }
