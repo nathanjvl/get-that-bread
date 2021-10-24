71ddb397f6584bbf00a5325b72d55d4e27f19c81
Nathan Lacsamana <nlacsama@uci.edu>
2021-10-23 18:17:58 -0700

    when hit goes away

---
+++ get-that-bread/Assets/Scripts/HitPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
