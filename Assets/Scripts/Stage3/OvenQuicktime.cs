a78879abd95b89e94b70f7278f1f2a53da4f0be6
Nathan Lacsamana <nlacsama@uci.edu>
2021-10-24 14:04:57 -0700

    Added OvenQuicktime Script

--- Assets/Scripts/Stage3/OvenQuicktime.cs
+++ Assets/Scripts/Stage3/OvenQuicktime.cs
@@ -8,11 +8,15 @@ public class OvenQuicktime : MonoBehaviour
     public Animator ovenAnim;
     public Stage3Manager s3manager;
 
-    public float window_time = 2f;
-    public float randomTime = 5f; // will be the time that counts down
+    public float window_time = 2f; // How long each window should be
+    public float qt_timer = 2f; // should start at window_time and count down
+
+    public float randomTime = 5f; // Varying time between quicktimes
 
     public float time_min = 5f;
     public float time_max = 10f;
+
+    public bool active_quicktime = false;
 
 
     // Start is called before the first frame update
@@ -25,21 +29,61 @@ public class OvenQuicktime : MonoBehaviour
     void Update()
     {
 
-
-        if (randomTime > 0)
+        if (active_quicktime == false)
         {
-            randomTime -= Time.deltaTime;
+            // No active quicktime // setting the time to see when the next one
+            // should activate
+            if (randomTime > 0)
+            {
+                randomTime -= Time.deltaTime;
+            } else
+            {
+                // start quick time event
+                Debug.Log("Quicktime is starting");
+                randomTime = Random.Range(time_min, time_max);
+                qtStart();
+
+            }
+        } else if (active_quicktime && Input.GetKeyDown(KeyCode.G))
+        {
+            //This means they pressed the button correctly
+            qtSucceed();
+
         } else
         {
-            // start quick time event
-            randomTime = Random.Range(time_min, time_max);
-
-
-        }
-
-
-        if(Input.GetKeyDown(KeyCode.Space)){
-
+            // This case means there is an active quicktime;
+            if (qt_timer > 0)
+            {
+                qt_timer -= Time.deltaTime;
+            } else
+            {
+                // This means the timer hit 0;
+                qtTimerRunOut();
+            }
         }
     }
+
+    //a quicktime event has start
+    void qtStart()
+    {
+        qt_timer = window_time;
+        active_quicktime = true;
+        ovenAnim.SetBool("is_Fire", true);
+    }
+
+    //this is what happens when the timer runs out
+    void qtTimerRunOut()
+    {
+        active_quicktime = false;
+        ovenAnim.SetBool("is_Fire", false);
+        s3manager.take_damage();
+    }
+
+    //this is what happens the player successfully clicks the button
+    void qtSucceed()
+    {
+        active_quicktime = false;
+        ovenAnim.SetBool("is_Fire", false);
+    }
+
 }
