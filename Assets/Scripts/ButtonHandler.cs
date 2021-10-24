01cd18e520495c919fae59d4920f022a5ebbc979
Nathan Lacsamana <nlacsama@uci.edu>
2021-10-23 23:29:06 -0700

    Added New Scenes and Transitions
    Added scenes for all the stages of the game as well as started uI implementation of transitions and menus

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void TransitionTitleScreen(){
+        SceneManager.LoadScene("Title Screen");
+    }
+
+}
