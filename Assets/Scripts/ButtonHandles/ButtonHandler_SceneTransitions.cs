8b296ab73de65a24c4f7d222a5caa6511463d2c4
Nathan Lacsamana <nlacsama@uci.edu>
2021-10-24 11:06:29 -0700

    Working on Scene transitions

--- Assets/Scripts/ButtonHandles/ButtonHandler_SceneTransitions.cs
+++ Assets/Scripts/ButtonHandles/ButtonHandler_SceneTransitions.cs
@@ -7,15 +7,18 @@ using UnityEngine.SceneManagement;
 // This script is meant to be added to buttons in order to access the
 // functions within this script.
 
+
 public class ButtonHandler_SceneTransitions : MonoBehaviour
 {
     //Changes the scene to the title screen
     public void TransitionTitleScreen(){
+        Debug.Log("Transitioning to Title Screen");
         SceneManager.LoadScene("Title Screen");
     }
 
     //Changes the scene to the Art Scene between Title and Stage 1
     public void TransitionArt1(){
+        Debug.Log("Transitioning to Art1");
         SceneManager.LoadScene("ArtTitleToStage1");
     }
 
--- Assets/Scripts/ButtonMash/ButtonMashSceneManager.cs
+++ Assets/Scripts/ButtonMash/ButtonMashSceneManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+//The ButtonMash Scene Manager
 public class ButtonMashSceneManager : MonoBehaviour
 {
+
+    // The transition animator that is used for the fade in and out;
+    public Animator transition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // THIS IS TEMPORARY SO WE CAN GET THROUGH ALL scenes
+        StartCoroutine(TransitionToWin());
+        // DELETE THE LINE ABOVE CALLING WIN
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    //Call when the player wins
+    IEnumerator TransitionToWin(){
+        transition.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1); // pauses for 1 second
+        SceneManager.LoadScene("WinScreen");
+    }
+
+    //Call when the player fails
+    IEnumerator TransitionGameOver(){
+        transition.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameOver");
+    }
+
+
 }
