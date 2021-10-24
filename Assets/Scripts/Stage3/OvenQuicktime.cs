698d51ecd391f0bbc7a4f6482f0d4ad291844e5f
Nathan Lacsamana <nlacsama@uci.edu>
2021-10-24 13:47:57 -0700

    added sprite

--- Assets/Scripts/Stage3/OvenQuicktime.cs
+++ Assets/Scripts/Stage3/OvenQuicktime.cs
@@ -9,7 +9,11 @@ public class OvenQuicktime : MonoBehaviour
     public Stage3Manager s3manager;
 
     public float window_time = 2f;
-    public float timer; // will be the time that counts down 
+    public float randomTime = 5f; // will be the time that counts down
+
+    public float time_min = 5f;
+    public float time_max = 10f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,20 @@ public class OvenQuicktime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        if (randomTime > 0)
+        {
+            randomTime -= Time.deltaTime;
+        } else
+        {
+            // start quick time event
+            randomTime = Random.Range(time_min, time_max);
+
+
+        }
+
+
         if(Input.GetKeyDown(KeyCode.Space)){
 
         }
