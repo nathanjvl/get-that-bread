9ccdaab1c974b40508bb70c133eb150a4cc18c12
Nathan Lacsamana <nlacsama@uci.edu>
2021-10-24 17:06:34 -0700

    Set up more art things

--- Assets/Scripts/ArtSceneManagers/ArtS1toS3Manager.cs
+++ Assets/Scripts/ArtSceneManagers/ArtS1toS3Manager.cs
@@ -10,23 +10,52 @@ public class ArtS1toS3Manager : MonoBehaviour
     // set their triggers and values so that we can have the camera
     // and other set pieces move around in time.
 
+    private int currentScene;
+    public float currentSceneMultiplier;
+
+    private float timer;
+
+    public GameObject c;
+
     // The transition animator that is used for the fade in and out;
     public Animator transition;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(ArtS12S3()); // this will call the IEnumerator and start it
+        timer = 2;
+
+        if (timer <= 0)
+        {
+            timer = 2;
+            currentScene++;
+        }
+
+        //StartCoroutine(ArtT2S1()); this will call the IEnumerator and start it
     }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = 2;
+            currentScene++;
+        }
+
+        c.transform.position = Vector3.Lerp(c.transform.position, new Vector3(currentScene * currentSceneMultiplier, 0, -10), Time.deltaTime * 5);
+
+        if (currentScene >= 2)
+        {
+            StartCoroutine(ArtS12S3()); // this will call the IEnumerator and start it
+        }
+
+    }
+
 
     // This is where we can handle different timing to direct the camera
     IEnumerator ArtS12S3(){
-
-        yield return new WaitForSeconds(1); // waiting for 2 seconds
-
-        // THIS IS WHERE TO PUT HOW WE NAVIGATE THE ART AND ADD MORE PAUSES
-
-
 
         //End Sequence
         transition.SetTrigger("FadeOut"); // sets the variable to active the fade out transition
